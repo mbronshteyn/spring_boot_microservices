df6e50bc36e2910a2ec654a4c5795c738b293d6f
mbronshteyn <michael.bronshteyn@gmail.com>
2019-05-16 17:38:20 -0700

    Add Exception Handler

---
+++ src/main/java/com/mbronshteyn/mobileappws/exceptions/AppExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.mbronshteyn.mobileappws.exceptions;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class AppExceptionHandler extends ResponseEntityExceptionHandler {
+    public ResponseEntity<Object> handleAnyException(Exception ex, WebRequest webRequest ){
+        return new ResponseEntity<>( ex, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR );
+    }
+}
